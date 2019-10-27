4ad6d8e2635a3f485704f7d166508ae94700f2fe
Jonathan Sack <jonathan.sack91@gmail.com>
2019-10-27 13:47:54 -0400

    Initial Commit

---
+++ Flash Feed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flash Feed
+//
+//  Created by Jonathan Sack on 10/27/19.
+//  Copyright © 2019 Jonathan Sack. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

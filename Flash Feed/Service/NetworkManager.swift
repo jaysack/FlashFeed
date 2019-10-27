a4d7adbed48d869f3c9026a70ad0a52e2e19d702
Jonathan Sack <jonathan.sack91@gmail.com>
2019-10-27 14:33:23 -0400

    Unabled .gitignore

---
+++ Flash Feed/Service/NetworkManager.swift
@@ -0,0 +1,31 @@
+//
+//  NetworkManager.swift
+//  Flash Feed
+//
+//  Created by Jonathan Sack on 10/27/19.
+//  Copyright © 2019 Jonathan Sack. All rights reserved.
+//
+
+import Foundation
+
+final class NetworkManager {
+    
+    // MARK: - Singleton Stack
+    static let shared = NetworkManager()
+    private init() {}
+    
+    
+    // MARK: - Fetch Sources
+    func fetchSources() {
+        
+        // Format URL
+        
+        // Start URLSession
+        
+            // Handle error(s)
+        
+            // Unwrap Data
+        
+            // Decode JSON
+    }
+}
---
+++ Flash Feed/Utilities/Private.swift
@@ -0,0 +1,17 @@
+//
+//  Private.swift
+//  Flash Feed
+//
+//  Created by Jonathan Sack on 10/27/19.
+//  Copyright © 2019 Jonathan Sack. All rights reserved.
+//
+
+import Foundation
+
+struct API {
+
+    struct KEY {
+        
+        static let NEWS = ""
+    }
+}
